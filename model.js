32fc2aee601a96e32ddb5871dde71975bc092247
tpinaa <tatiana.dcp@gmail.com>
2024-08-04 12:08:03 -0300

    Criados Schema Indicador e Publicacao|Validacoes Schema Usuarios

--- model.js
+++ model.js
@@ -1,10 +1,46 @@
 import { Schema, model } from "mongoose"
-const UserSchema = new Schema(
+
+const UsuarioSchema = new Schema(
     {
-        nome: String,
-        senha: String,
+        nome: {
+            type: String,
+            required: [true, 'Insira um nome']},
+        senha: {
+            type: String,
+            minLength: [6, 'Senha deve conter no mínimo 6 caracteres']},
         perfil: String,
     }
 )
 
-export const User = model('User', UserSchema)
+export const Usuario = model('Usuario', UsuarioSchema)
+
+const IndicadorSchema = new Schema(
+    {
+        nome: String,
+        formula: String,
+        unidade: String,
+        descricao: String,
+        objetivo: String,
+        observacao: String,
+        justificativa: String,
+        comprovacao: String,
+        fonte: String,
+    }
+)
+
+export const Indicador = model('Indicador', IndicadorSchema)
+
+const PublicacaoSchema = new Schema(
+    {
+        titulo: {type: String, required: true},
+        projeto: String,
+        situacao: Boolean,
+        dataRegistro: Date,
+        dataPublicacao: Date,
+        autor: String,
+        tipo: String,
+        classificacao: String,
+    }
+)
+
+export const Publicacao = model('Publicacao', PublicacaoSchema)
