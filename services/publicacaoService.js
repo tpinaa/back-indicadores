95d7be675341c3b49d58146d19f0075b89da4041
tpinaa <tatiana.dcp@gmail.com>
2024-11-30 17:56:10 -0300

    implementado serviços de publicação

--- services/publicacaoService.js
+++ services/publicacaoService.js
@@ -1,23 +1,35 @@
-import { Publicacao } from "../model.js";
+import { Publicacao } from '../model.js';
 
-export async function criarPublicacao(
+export async function criarPublicacao(titulo, dataPublicacao, autor, tipo) {
+  const publicacao = await Publicacao.create({
     titulo,
-    projeto,
-    situacao,
-    dataRegistro,
     dataPublicacao,
     autor,
     tipo,
-    classificacao) {
-    const publicacao = await Publicacao.create({
-        titulo,
-        projeto,
-        situacao,
-        dataRegistro,
-        dataPublicacao,
-        autor,
-        tipo,
-        classificacao
-    })
+  });
+  return publicacao;
+}
+
+export async function buscarPublicacoes() {
+  const lista = await Publicacao.find();
+  return lista;
+}
+
+export async function buscarPublicacaoPorId(idPublicacao) {
+    const publicacao = await Publicacao.findById(idPublicacao)
+    return publicacao
+}
+
+export async function atualizarPublicacaoPorId(idPublicacao, dadosAtualizados) {
+    const publicacaoAtualizada = await Publicacao.findByIdAndUpdate(
+        idPublicacao,
+        dadosAtualizados,
+        { new: true }
+    )
+    return publicacaoAtualizada
+}
+
+export async function deletarPublicacaoPorId(idPublicacao) {
+    const publicacao = await Publicacao.findByIdAndDelete(idPublicacao)
     return publicacao
 }
