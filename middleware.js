e8756c6233e7298158dd6c93dce07254ddea3a3c
tpinaa <tatiana.dcp@gmail.com>
2024-11-30 17:59:43 -0300

    validações do middleware de autenticacao

--- middleware.js
+++ middleware.js
@@ -7,20 +7,26 @@ export async function tratamentoErros(err, req, res, next) {
             .map(erro => erro.message)
             .join('; ')
         res.json({ erro: listaErros })
-    } else {
+    } 
+    else {
         res.json({ erro: err.message })
     }
 }
 
 export async function autenticacao(req, res, next) {
     const authHeader = req.headers['authorization']
+    if(!authHeader) {
+        next(new Error('Token não providenciado'))
+        return
+    }
+
     const [_, token] = authHeader.split(' ')
 
     try {
         const payload = jwt.verify(token, "chave_secreta")
         req.userId = payload.id
         next()
-    } catch(err) {
-        next(err)
+    } catch {
+        next(new Error('Token inválido ou expirado'))
     }
 }
