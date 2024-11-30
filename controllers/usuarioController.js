ecaf22ae95f7207a3f5eca5ead3c8e995089b066
tpinaa <tatiana.dcp@gmail.com>
2024-11-30 17:49:34 -0300

    tratamento de erros | middleware autenticacao | resposta em JSON

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,17 +1,31 @@
 import express from "express"
-import { criarUsuario, autenticarUsuario, buscarUsuarios, buscarUsuarioPorId, deletarUsuarioPorId, atualizarUsuarioPorId } from "../services/usuarioService.js"
+import { 
+  criarUsuario,
+  autenticarUsuario,
+  buscarUsuarios,
+  buscarUsuarioPorId,
+  deletarUsuarioPorId,
+  atualizarUsuarioPorId } from "../services/usuarioService.js"
 import { autenticacao } from "../middleware.js"
 const router = express.Router()
 
-router.get('/', autenticacao, async (req, res) => {
-  const listaUsuarios = await buscarUsuarios()
-  res.send(listaUsuarios)
+router.get('/', autenticacao, async (req, res, next) => {
+  try {
+    const listaUsuarios = await buscarUsuarios()
+    res.json(listaUsuarios)
+  } catch(err) {
+    next(err)
+  }
 })
 
-router.get('/:id', autenticacao, async (req, res) => {
+router.get('/:id', autenticacao, async (req, res, next) => {
   const id = req.params.id
-  const usuario = await buscarUsuarioPorId(id)
-  res.send(usuario)
+  try {
+    const usuario = await buscarUsuarioPorId(id)
+    res.json(usuario)
+  } catch(err) {
+    next(err)
+  }
 })
 
 router.post('/', async (req, res, next) => {
@@ -34,17 +48,25 @@ router.post('/autenticacao', async (req, res, next) => {
   }
 })
 
-router.delete('/:id', autenticacao, async (req, res) => {
+router.delete('/:id', autenticacao, async (req, res, next) => {
   const id = req.params.id
-  const usuario = await deletarUsuarioPorId(id)
-  res.json(usuario)
+  try {
+    const usuario = await deletarUsuarioPorId(id)
+    res.json(usuario)
+  } catch(err) {
+    next(err)
+  }
 })
 
-router.put('/:id', autenticacao, async (req, res) => {
+router.put('/:id', autenticacao, async (req, res, next) => {
   const id = req.params.id
   const { nome, senha, perfil } = req.body
-  const usuarioAtualizado = await atualizarUsuarioPorId(id, { nome, senha, perfil })
-  return res.send(usuarioAtualizado)
+  try {
+    const usuarioAtualizado = await atualizarUsuarioPorId(id, { nome, senha, perfil })
+    res.json(usuarioAtualizado)
+  } catch(err) {
+    next(err)
+  }
 })
 
 export default router
