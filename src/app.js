64bc608a3200cd76bbffab946d269219e473574d
tpinaa <tatiana.dcp@gmail.com>
2025-09-29 20:23:17 -0300

    refatoração do código

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from 'cors'
 import usuarioRouter from "./controllers/usuarioController.js"
 import indicadorRouter from "./controllers/indicadorController.js"
 import publicacaoRouter from "./controllers/publicacaoController.js"
@@ -6,6 +7,13 @@ import parametroRouter from "./controllers/parametroController.js"
 import { tratamentoErros } from "./middleware.js"
 
 const app = express()
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST, PUT, DELETE',
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use('/', express.json())
 app.use('/usuario', usuarioRouter)
